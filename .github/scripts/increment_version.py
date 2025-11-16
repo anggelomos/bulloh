d2c98d1041fcd456d0f277f629f8ef2685cb6bcc
Angelo Mosquera <anggelomos@outlook.com>
2025-11-16 12:47:53 -0500

    Add poetry

--- .github/scripts/increment_version.py
+++ .github/scripts/increment_version.py
@@ -8,7 +8,7 @@ package_remote_version = version("bulloh")
 with open(pyproject_path, 'r') as file:
     pyproject_data = toml.load(file)
 
-package_local_version = pyproject_data["project"]["version"]
+package_local_version = pyproject_data["tool"]["poetry"]["version"]
 
 package_remote_version_split = package_remote_version.split(".")
 package_local_version_split = package_local_version.split(".")
@@ -19,7 +19,7 @@ if (package_remote_version_split[0] == package_local_version_split[0]) and \
     package_remote_version_split[2] = str(int(package_remote_version_split[2]) + 1)
     package_version = ".".join(package_remote_version_split)
 
-pyproject_data["project"]["version"] = package_version
+pyproject_data["tool"]["poetry"]["version"] = package_version
 
 with open(pyproject_path, 'w') as file:
     toml.dump(pyproject_data, file)
